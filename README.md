9ad77b2501f8d79019c5cdb68ca3c9c1b53827b0
sleepingkingstudios <merlin@sleepingkingstudios.com>
2015-03-07 15:28:11 -0500

    Add assets for Twitter Bootstrap framework.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # sleepingkingstudios.github.io
 GitHub Pages for Sleeping King Studios
+
+## Local Development
+
+```
+# Run a local server on port 4000.
+jekyll serve --watch
+```
---
+++ assets/javascripts.js
@@ -0,0 +1,8 @@
+---
+layout: nil
+---
+
+/* Included files. */
+
+{% include assets/javascripts/jquery-2.1.3.min.js %}
+{% include assets/javascripts/twitter-bootstrap/bootstrap.min.js %}
